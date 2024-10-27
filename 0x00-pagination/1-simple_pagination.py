b86c2238455277caff786ed8236c417d60fe74d2
Doctware <yusclever9@gmail.com>
2024-10-27 17:51:41 +0100

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" this module implement simple oagination """
+import math
+import csv
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to peginate a database of populat baby names
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cashed dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        The mehod thats implement a simple pagination
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_idx >= len(dataset):
+            return []
+        return dataset[start_idx:end_idx]
