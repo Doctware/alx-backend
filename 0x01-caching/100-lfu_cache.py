9efdfc2c0c330fbd52f66e038d0676086418aeaa
Doctware <yusclever9@gmail.com>
2024-10-30 22:56:26 +0100

    let do LFU cache

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" this module implement cache replacement sysytem for LFU """
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ the class MRU cache thats inherit from Base caching """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.usage_tracker = []
+
+    def put(self, key, item):
+        """
+        The put methos:
+
+        assign to dictionary self.cached_data the item value
+        for the key
+
+        if key or item is None then do nothing
+
+        if lenght of cache data is > than BaseCahing.MAX_ITEMS
+        remove the leat frquently item i.e perform LFU algorithim
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.usage_tracker.remove(key)
+
+        self.cache_data[key] = item
+        self.usage_tracker.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            mru_item = self.usage_tracker.pop(-1)
+            del self.cache_data[mru_item]
+            print("DISCARD: {}".format(mru_item))
+
+    def get(self, key):
+        """
+        returns the value in cache_data linked to key
+        if key is None or not in cache_data return noen
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.usage_tracker.remove(key)
+        self.usage_tracker.append(key)
+        return self.cache_data[key]
