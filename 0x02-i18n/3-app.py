951ccc7216b26670fa9f575d470dd6f0b1e7ad5e
Doctware <yusclever9@gmail.com>
2024-11-06 03:04:15 +0100

    parameterize the template

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" The flak app """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _i
+from typing import Optional
+
+app = Flask(__name__)
+
+
+class Config:
+    """ the config class for babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """ this function determin the best mach with supported language """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    this funtion simply output a text
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
